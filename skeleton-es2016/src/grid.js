efa101cad8d59213b1e39fbcfe1bc0c0cf88e6e0
Chris Rock <chris@rocknetconsulting.com>
2016-04-30 15:00:18 -0400

    Pro through step 2.2.3

---
+++ skeleton-es2016/src/grid.js
@@ -0,0 +1,16 @@
+export class BasicUse {
+  pageable = {
+    refresh: true,
+    pageSizes: true,
+    buttonCount: 10
+  };
+  constructor() {
+    this.datasource = {
+      type: 'odata',
+      transport: {
+        read: '//demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'
+      },
+      pageSize: 10
+    };
+  }
+}
