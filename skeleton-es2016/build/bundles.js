a84ddac8b96b9a795e6214c4e255c37125309523
Chris Rock <chris@rocknetconsulting.com>
2016-05-12 13:52:45 -0400

    Bridge bundled with pro.

--- skeleton-es2016/build/bundles.js
+++ skeleton-es2016/build/bundles.js
@@ -12,8 +12,32 @@ module.exports = {
       "options": {
         "inject": true,
         "minify": true,
-        "depCache": true,
+        "depCache": false,
         "rev": false
+      }
+    },
+    "dist/kendo-build": {
+      "includes": [ 
+        "kendo-ui/js/*.min.js",
+        "kendo-ui/styles/kendo.common.min.css!text",
+        "kendo-ui/styles/kendo.bootstrap.min.css!text" 
+      ],
+      "excludes": [
+        "kendo-ui/js/angular.min.js",
+        "kendo-ui/js/jquery.min.js",
+        "kendo-ui/js/kendo.angular.min.js",
+        "kendo-ui/js/kendo.angular2.min.js",
+        "kendo-ui/js/kendo.spreadsheet.min.js",
+        "kendo-ui/js/kendo.all.min.js",
+        "kendo-ui/js/kendo.web.min.js",
+        "kendo-ui/js/kendo.dataviz.min.js",
+        "kendo-ui/js/kendo.dataviz.mobile.min.js",
+        "kendo-ui/js/kendo.mobile.min.js"
+      ],
+      "options": {
+        "inject": true,
+        "minify": true,
+        "rev": true
       }
     },
     "dist/aurelia": {
