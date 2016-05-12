150ececbcd5308e7a6c8f4969410bcfc6b9051b0
Chris Rock <chris@rocknetconsulting.com>
2016-05-12 13:53:09 -0400

    Don't do a new bundle every time we serve it.

--- skeleton-es2016/build/tasks/serve.js
+++ skeleton-es2016/build/tasks/serve.js
@@ -22,7 +22,7 @@ gulp.task('serve', ['build'], function(done) {
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve-bundle', ['bundle'], function(done) {
+gulp.task('serve-bundle', function(done) {
   browserSync({
     online: false,
     open: false,
